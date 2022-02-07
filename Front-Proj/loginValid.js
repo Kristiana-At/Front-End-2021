b71d36ce265b157b59ff42190c1f4ccbfbc067d0
Kristiana-At <77620189+Kristiana-At@users.noreply.github.com>
2022-02-07 11:04:00 +0200

    first

---
+++ Front-Proj/loginValid.js
@@ -0,0 +1,123 @@
+function checkIfExist()
+{
+    var storedUsersCheck = new Array();
+    storedUsersCheck = JSON.parse((localStorage.getItem("users")));
+
+    var usernameCheck = document.getElementById('username').value;
+    for(let i = 0; i < storedUsersCheck.length; i++)
+    {
+        if(usernameCheck == storedUsersCheck[i].username)
+        { 
+            return true;
+        }
+    }
+    return false;
+}
+
+function store(){
+    var a = new Array();
+    user = new Object();
+
+    var username = document.getElementById('username');
+    var password = document.getElementById('password');
+    var lowerCaseLetters = /[a-z]/g;
+    var containCharacteer = /@/;
+    var upperCaseLetters = /[A-Z]/g;
+    var numbers = /[0-9]/g;
+
+    user={
+        username: username.value,
+        password: password.value
+    };
+
+    if(localStorage.length)
+    {
+    a=JSON.parse((localStorage.getItem("users")));
+    }
+
+    a.push(user);
+
+    if(checkIfExist()){
+        alert("User already exists!");
+    }
+    else if(username.value.length == 0 && password.value.length == 0){ 
+        alert('Please fill in email'); 
+
+    }else if(username.value.length == 0){
+        alert('Please fill in password');       
+
+    }else if(password.value.length == 0){
+        alert('Please fill in email and password');       
+
+    }else if(!username.value.match(containCharacteer)){
+        alert('Invalid e-mail');       
+
+    }else if(password.value.length < 8){
+        alert('Minimum of 8 characters');        
+
+    }else if(!password.value.match(numbers)){
+        alert('please add 1 number');      
+
+    }else if(!password.value.match(upperCaseLetters)){
+        alert('please add 1 uppercase letter');        
+
+    }else if(!password.value.match(lowerCaseLetters)){
+        alert('please add 1 lowercase letter');        
+
+    }else{;
+        localStorage.setItem('users', JSON.stringify(a));
+        alert('Your account has been created');
+        document.changeAction.action = 'login.html';
+       // window.open("login.html");
+       //window.location = "login.html";
+    }
+}
+
+function check(){
+    var storedUsers = new Array();
+    storedUsers = JSON.parse((localStorage.getItem("users")));
+
+    var logUsername = document.getElementById('logUsername').value;
+    var logPassword = document.getElementById('logPassword').value;
+
+    for(let i = 0; i < storedUsers.length; i++)
+    {
+        if(logUsername == storedUsers[i].username && logPassword == storedUsers[i].password){
+
+            document.getElementsById("sub").addEventListener("onclick", function(event){
+                event.preventDefault()
+              });
+            //document.changeAction.action = 'movie_list.html';
+            window.location.href = "movie_list.html";
+            //window.location = "movie_list_noJSON.html";
+            // window.open('movie_list.html');
+            return;
+        }
+    }
+    alert('Error on login');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //var userRemember = document.getElementById("rememberMe");
+// var i;
+
+// for (i = 0; i < localStorage.length; i++)   {
+//     console.log(i + localStorage.key(i) + "=[" + localStorage.getItem(localStorage.key(i)) + "]");
+// }
+
+// function deleteItems() {
+//     localStorage.clear();
+//   }
+  
---
+++ Front-Proj/scriptMain.js
@@ -0,0 +1,55 @@
+fetch('./data.json')
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                appendData(data);
+            })
+            .catch(function (err) {
+                console.log('error: ' + err);
+            });
+        function appendData(data) {
+            var mainContainer = document.getElementById("main");
+            data.sort((a, b) => Number(b.year) - Number(a.year)); //sort descending
+            for (var i = 0; i < data.length; i++) {
+                var div = document.createElement("div");
+                div.className ="movie";
+
+                var img = document.createElement("img");
+                
+
+                var div2 = document.createElement("div");
+                div2.className = "image_cover";
+                
+
+                var p = document.createElement("p");
+                p.className = "description";
+                
+
+                var div3 = document.createElement("div")
+                div3.className = "movie-info";
+                
+
+                var a = document.createElement("a");
+                a.className = "link";
+
+                var span = document.createElement("span");
+                
+
+                img.src = data[i].image;
+                p.innerHTML = data[i].description;
+                a.href = "login.html";
+                a.innerHTML = data[i].title;
+                span.innerHTML = data[i].year;
+
+                div.appendChild(img);
+                div.appendChild(div2);
+                div2.appendChild(p);
+                div.appendChild(div3);
+                div3.appendChild(a);
+                div3.appendChild(span);
+                mainContainer.appendChild(div);
+            }
+        }
+
+       
